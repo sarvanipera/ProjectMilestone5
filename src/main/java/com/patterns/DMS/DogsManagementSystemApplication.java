ebddb9a4ce76180112fae3fb691cb8f00d96ae1b
perasarvani <sarvanipera123@gmail.com>
2023-11-14 22:36:41 -0600

    files added

---
+++ src/main/java/com/patterns/DMS/DogsManagementSystemApplication.java
@@ -0,0 +1,16 @@
+package com.patterns.DMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+/*
+ * @Sarvani Pera
+ */
+public class DogsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsManagementSystemApplication.class, args);
+	}
+
+}
